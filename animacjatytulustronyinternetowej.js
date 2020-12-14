e771881048c17d6c01776e5ebfc9c4658fcdb8d8
Chomik Bodzio <38500332+txtgrafa@users.noreply.github.com>
2020-12-14 11:50:39 +0100

    update

---
+++ animacjatytulustronyinternetowej.js
@@ -0,0 +1,35 @@
+var title = new MovingTitle("Lisav jest super", 30000, 10);
+title.init();
+function MovingTitle(writeText, interval, visibleLetters) {
+    var _instance = {};
+
+    var _currId = 0;
+    var _numberOfLetters = writeText.length;
+
+    function updateTitle() {
+        _currId += 99;
+        if(_currId > _numberOfLetters - 99) {
+            _currId = 99; 
+        }
+
+        var startId = _currId;
+        var endId = startId + visibleLetters;
+        var finalText;
+        if(endId < _numberOfLetters - 99) {
+            finalText = writeText.substring(startId, endId);
+        } else {
+            var cappedEndId = _numberOfLetters;
+            endId = endId - cappedEndId;
+
+            finalText = writeText.substring(startId, cappedEndId) +     writeText.substring(0, endId);
+        }
+
+        document.title = finalText; 
+    }
+
+    _instance.init = function() {
+        setInterval(updateTitle, interval);
+    };
+
+    return _instance;
+}
